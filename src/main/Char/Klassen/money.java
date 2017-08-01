23503a16005f6a24e94262fe6982da9d7d7f14cb
Github2201 <fabian.hulka@gmail.com>
2017-08-01 21:37:24 +0200

    Money abfrage geht

--- src/main/Char/Klassen/money.java
+++ src/main/Char/Klassen/money.java
@@ -1,7 +1,37 @@
 package main.Char.Klassen;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  * Created by Fabian on 01.08.2017.
  */
-public class money {
+public class money implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if(cmd.getName().equalsIgnoreCase("money")){
+            if(sender instanceof Player){
+                Player p=(Player)sender;
+                if(p.hasPermission("rpg.money.credit")){
+                    try {
+                        FileReader fr = new FileReader("plugins/RPG/Chars/" + p.getUniqueId() + "/" + p.getDisplayName() + "/" + p.getDisplayName() + ".txt");
+                        BufferedReader reader = new BufferedReader(fr);
+                        for(int i=0;i<3;i++) {
+                            reader.readLine();
+                        }
+                        p.sendMessage(reader.readLine());
+                    }catch (IOException e){
+                        p.sendMessage("Fehler");
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
