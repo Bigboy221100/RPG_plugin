a601c74a9da3c4fef5911d9996beeeb7c21ed831
BuildTools <unconfigured@null.spigotmc.org>
2017-08-01 21:27:16 +0200

    Minigame Test

--- src/main/Minigame/Minigame.java
+++ src/main/Minigame/Minigame.java
@@ -8,12 +8,28 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Minigame implements CommandExecutor, Listener {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         Player p = (Player) commandSender;
-        p.sendMessage("Du hast ein Minigame gestartet.");
+        String minigameName="";
+        if(args.length >= 2) {
+            if (args[2].equals("erstellen")) {
+                minigameName = args[3];
+                try(BufferedWriter writer = Files.newBufferedWriter(Paths.get("/RPG/Minigame/"), Charset.forName("UTF-8"))){
+                    writer.write(minigameName);
+                } catch (IOException e) {
+                    p.sendMessage("Das Minigame konnte nicht erstellt werden!");
+                }
+            }
+        }
         return false;
     }
 }
