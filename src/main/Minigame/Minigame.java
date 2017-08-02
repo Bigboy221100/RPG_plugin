02f58a80e5359d383c302b1be7671c0d90082be7
BuildTools <unconfigured@null.spigotmc.org>
2017-08-02 21:30:10 +0200

    Minigame Konstrukt 9

--- src/main/Minigame/Minigame.java
+++ src/main/Minigame/Minigame.java
@@ -2,6 +2,7 @@ package main.Minigame;
 
 
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -37,7 +38,7 @@ public class Minigame implements CommandExecutor, Listener{
                     }
                     try(BufferedWriter writer = Files.newBufferedWriter(Paths.get("plugins/RPG/MinigameTest/minigame.txt"), Charset.forName("UTF-8"))) {
                         writer.write(gesamt);
-                        writer.write(minigameName + " 0" + " 0");
+                        writer.write(minigameName + " 0" + " 0" + " 0" + " 0" + " 0" + " 0" );
                     } catch(IOException e) {
                         p.sendMessage(e.toString());
                     }
@@ -45,6 +46,9 @@ public class Minigame implements CommandExecutor, Listener{
                 } else {
                     p.sendMessage("Bitte gib /minigame erstellen (Minigame-Name) ein.");
                 }
+
+
+                //Minigame löschen
                 if (args[0].equalsIgnoreCase("löschen")) {
                     p.sendMessage("Versuche Minigame zu löschen ...");
                     String gesamt="";
@@ -67,6 +71,10 @@ public class Minigame implements CommandExecutor, Listener{
                         p.sendMessage(e.toString());
                     }
                 }
+
+
+
+            //Minigame list
             } else if (args.length <= 1) {
                 if (args[0].equalsIgnoreCase("list")) {
                     try(BufferedReader reader = Files.newBufferedReader(Paths.get("plugins/RPG/MinigameTest/minigame.txt"), Charset.forName("UTF-8"))){
@@ -82,11 +90,43 @@ public class Minigame implements CommandExecutor, Listener{
                     p.sendMessage("Bitte gib /minigame erstellen (Minigame-Name) ein.");
                 }
             }
+
+
+
             //MiniGame Punkte erstellen
-            if(args.length == 2) {
+            if(args.length == 3) {
                 if (args[0].equalsIgnoreCase("Punkt")) {
-
-
+                    Location targetloc = p.getLocation();
+                    String minigame[]=new String[4];
+                    try(BufferedReader reader = Files.newBufferedReader(Paths.get("plugins/RPG/MinigameTest/minigame.txt"), Charset.forName("UTF-8"))){
+                        String line="";
+                        while((line=reader.readLine()) != null){
+                            String[]minigameArray=line.split(" ");
+                            if(args[1].equalsIgnoreCase(minigameArray[0])) {
+                                minigame[0]=minigameArray[0];
+                                minigame[1]=minigameArray[1];
+                                minigame[2]=minigameArray[2];
+                                minigame[3]=minigameArray[3];
+                                minigame[4]=minigameArray[4];
+                                minigame[5]=minigameArray[5];
+                                minigame[6]=minigameArray[6];
+                            }
+                        }
+                    } catch (IOException e) {
+                        p.sendMessage("Die Puntke konnten nicht gespeichert werden!");
+                    }
+                    if (args[2].equalsIgnoreCase("1")) {
+                        minigame[1]=targetloc.getBlockX()+"";
+                        minigame[2]=targetloc.getBlockY()+"";
+                        minigame[3]=targetloc.getBlockZ()+"";
+                        p.sendMessage(minigame[1]+minigame[2]+minigame[3]);
+                    }
+                    if (args[2].equalsIgnoreCase("2")) {
+                        minigame[4]=targetloc.getBlockX()+"";
+                        minigame[5]=targetloc.getBlockY()+"";
+                        minigame[6]=targetloc.getBlockZ()+"";
+                        p.sendMessage(minigame[4]+minigame[5]+minigame[6]);
+                    }
                 }
             }
         }
