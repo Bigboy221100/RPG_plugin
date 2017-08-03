f6352f7d39169b421238ad00ef0056804f69661e
Github2201 <fabian.hulka@gmail.com>
2017-08-03 18:26:50 +0200

    Issue unendlich bogen

--- src/main/Char/Klassen/Archer/Archerevents.java
+++ src/main/Char/Klassen/Archer/Archerevents.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
