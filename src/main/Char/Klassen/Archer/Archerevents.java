53943e5d9c80d15cb5d0c17db3bfb07ea21fc7a1
Github2201 <fabian.hulka@gmail.com>
2017-09-12 20:11:01 +0200

    Arbeite am Archer Levelsystem

--- src/main/Char/Klassen/Archer/Archerevents.java
+++ src/main/Char/Klassen/Archer/Archerevents.java
@@ -23,12 +23,11 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
  * Created by Fabian on 02.08.2017.
  */
 public class Archerevents implements Listener {
-    boolean bowupgrade=false;
     @EventHandler
     public void onLevel(PlayerLevelChangeEvent e) {
         Player p = e.getPlayer();
         helpvoids help = new helpvoids();
-        help.setcharacterlevel(p.getDisplayName(),p,e.getNewLevel());
+        help.setcharacterlevel(p.getDisplayName(), p, e.getNewLevel());
         if ((help.getcharacterklasse(p.getDisplayName() + "", p)).equalsIgnoreCase("Archer")) {
             p.sendMessage("Sie sind nun Level " + e.getNewLevel());
             switch (e.getNewLevel()) {
@@ -110,54 +109,65 @@ public class Archerevents implements Listener {
     @EventHandler
     public void onPlayerInvItemClick(InventoryClickEvent e) {
         Player p = (Player) e.getWhoClicked();
+        p.sendMessage(e.getSlot()+"");
+        Inventory inv = p.getServer().createInventory(null, 54, "Upgraden sie ihren Bogen");
+        ItemStack power[] = new ItemStack[5];
+        ItemStack punch[] = new ItemStack[5];
+        ItemStack flame[] = new ItemStack[2];
+
         if (e.getSlot() == 10) {
-            if(p.getLevel()>=5){
-                bowupgrades(p);
-                bowupgrade=true;
-            }else{
-                p.sendMessage("Ihr Bogen ist noch nicht zum Upgraden bereit");
+            ItemStack bow = new ItemStack(Material.BOW);
+            ItemMeta metabow = bow.getItemMeta();
+            metabow.setDisplayName("Bow upgraden");
+            bow.setItemMeta(metabow);
+            if (e.getInventory().getItem(10).equals(bow)) {
+                if (p.getLevel() >= 5) {
+                    for (int i = 0; i < 5; i++) {
+                        power[i] = new ItemStack(Material.BOOK);
+                        ItemMeta metapower = power[i].getItemMeta();
+                        metapower.setDisplayName("Power");
+                        power[i].setItemMeta(metapower);
+                        inv.setItem(2 + (i * 9), power[i]);
+                    }
+
+                    for (int i = 0; i < 5; i++) {
+                        punch[i] = new ItemStack(Material.BOOK);
+                        ItemMeta metapunch = power[i].getItemMeta();
+                        metapunch.setDisplayName("Punch");
+                        punch[i].setItemMeta(metapunch);
+                        inv.setItem(4 + (i * 9), punch[i]);
+                    }
+
+                    for (int i = 0; i < 2; i++) {
+                        flame[i] = new ItemStack(Material.BOOK);
+                        ItemMeta metaflame = power[i].getItemMeta();
+                        metaflame.setDisplayName("Flame");
+                        flame[i].setItemMeta(metaflame);
+                    }
+                    inv.setItem(33, flame[0]);
+                    inv.setItem(42, flame[1]);
+
+
+                    ItemStack skillpoints = new ItemStack(Material.NETHER_STAR, (p.getLevel() / 5));
+                    ItemMeta metaskill = skillpoints.getItemMeta();
+                    metaskill.setDisplayName("Skillpoints");
+                    skillpoints.setItemMeta(metaskill);
+                    inv.setItem(53, skillpoints);
+                    p.openInventory(inv);
+
+                } else {
+                    p.sendMessage("Ihr Bogen ist noch nicht zum Upgraden bereit");
+                }
             }
         }
-    }
-
-
-    public void bowupgrades(Player p) {
-        Inventory inv = p.getServer().createInventory(null, 54, "Upgraden sie ihren Bogen");
-
-        ItemStack power[] = new ItemStack[5];
-        for(int i=0;i<5;i++){
-            power[i]=new ItemStack(Material.BOOK);
-            ItemMeta metapower = power[i].getItemMeta();
+        if (e.getSlot() == 2) {
+            ItemMeta metapower = power[0].getItemMeta();
             metapower.setDisplayName("Power");
-            power[i].setItemMeta(metapower);
-            inv.setItem(2+(i*9), power[i]);
+            power[0].setItemMeta(metapower);
+            inv.setItem(2, power[0]);
+            p.sendMessage("yeah");
         }
-        ItemStack punch[] = new ItemStack[5];
-        for(int i=0;i<5;i++){
-            punch[i]=new ItemStack(Material.BOOK);
-            ItemMeta metapunch = power[i].getItemMeta();
-            metapunch.setDisplayName("Punch");
-            punch[i].setItemMeta(metapunch);
-            inv.setItem(4+(i*9), punch[i]);
-        }
-        ItemStack flame[] = new ItemStack[2];
-        for(int i=0;i<2;i++){
-            flame[i]=new ItemStack(Material.BOOK);
-            ItemMeta metaflame = power[i].getItemMeta();
-            metaflame.setDisplayName("flame");
-            flame[i].setItemMeta(metaflame);
-        }
-        inv.setItem(33, flame[0]);
-        inv.setItem(42, flame[1]);
-
-
-        ItemStack skillpoints = new ItemStack(Material.NETHER_STAR,(p.getLevel()/5));
-        ItemMeta metaskill=skillpoints.getItemMeta();
-        metaskill.setDisplayName("Skillpoints");
-        skillpoints.setItemMeta(metaskill);
-        inv.setItem(53,skillpoints);
-
-        p.openInventory(inv);
     }
+
 
 }
